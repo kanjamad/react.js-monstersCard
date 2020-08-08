1ce510934222e11e97e90b1c314b7c2599b9a5b6
kanjamad bosel <kanjamadpor@gmail.com>
2020-08-08 14:00:51 -0700

    Updates

---
+++ precache-manifest.0f70edb4e207daf5722f3d2959fd9dd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f93e99a9d6307527aef6624d973cc8",
+    "url": "/react.js-monstersCard/index.html"
+  },
+  {
+    "revision": "c5b788745f83d3ad7225",
+    "url": "/react.js-monstersCard/static/css/main.ae781e4a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react.js-monstersCard/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react.js-monstersCard/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b788745f83d3ad7225",
+    "url": "/react.js-monstersCard/static/js/main.5397a79f.chunk.js"
+  },
+  {
+    "revision": "3a4d8b7d18589c6fed4a",
+    "url": "/react.js-monstersCard/static/js/runtime-main.982571dc.js"
+  }
+]);
